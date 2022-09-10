0c0c7a06789733f9d3216414560994566407af46
Armani666 <abiram_sanser@hotmail.com>
2022-09-10 09:41:16 -0500

    Practica1-PWD-1003

---
+++ src/Repaso/01-tipos-datos.ts
@@ -0,0 +1,21 @@
+
+
+console.log('Hola Mundo!-pwd-1003');
+
+let nombre : string = "Ramses"; 
+let number : number =111;
+let acerdita : boolean=false;
+
+
+let apaterno: number | string = "Abimael" /* | -> se refiere a "O" */
+
+apaterno = 238;
+
+var miVariable = 'Hola';
+const tem=234;
+
+
+
+
+
+
